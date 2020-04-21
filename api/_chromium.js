53c23d874479be5a054bf1c5baecd490d64cd486
Rude <rude.ayelo@xing.com>
2020-04-21 10:59:48 +0200

    minor changes

--- api/_chromium.js
+++ api/_chromium.js
@@ -1,44 +1,46 @@
-import chrome from "chrome-aws-lambda"
-import puppeteer from "puppeteer-core"
+import chrome from "chrome-aws-lambda";
+import puppeteer from "puppeteer-core";
 
 export async function getHTML({ url, isDev }) {
-    const options = await getOptions(isDev)
-    const browser = await puppeteer.launch(options)
+  const options = await getOptions(isDev);
+  const browser = await puppeteer.launch(options);
 
-    const page = await browser.newPage()
-    await page.goto(url)
+  const page = await browser.newPage();
+  await page.goto(url);
 
-    const html = await page.evaluate(() => document.documentElement.innerHTML)
+  const html = await page.evaluate(() => document.documentElement.innerHTML);
 
-    await page.close()
-    await browser.close()
+  await page.close();
+  await browser.close();
 
-    return html
+  return html;
 }
 
 async function getOptions(isDev) {
-    if (isDev) {
-        return {
-            args: [],
-            executablePath: getLocalExePath(),
-            headless: true,
-        }
-    }
-
+  if (isDev) {
     return {
-        args: chrome.args,
-        executablePath: await chrome.executablePath,
-        headless: chrome.headless,
-    }
+      args: [],
+      executablePath: getLocalExePath(),
+      headless: true,
+    };
+  }
+
+  return {
+    // large height needed for lazy-loaded resources
+    defaultViewport: { width: 960, height: 10000 },
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: isDev ? false : chrome.headless,
+  };
 }
 
 function getLocalExePath() {
-    switch (process.platform) {
-        case "win32":
-            return "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-        case "linux":
-            return "/usr/bin/google-chrome"
-        default:
-            return "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-    }
+  switch (process.platform) {
+    case "win32":
+      return "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe";
+    case "linux":
+      return "/usr/bin/google-chrome";
+    default:
+      return "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+  }
 }
--- api/graphql.js
+++ api/graphql.js
@@ -1,88 +1,89 @@
-import { ApolloServer, gql } from "apollo-server-micro"
-import cheerio from "cheerio"
+import { ApolloServer, gql } from "apollo-server-micro";
+import cheerio from "cheerio";
 
-import { getHTML } from "./_chromium"
+import { getHTML } from "./_chromium";
 
-const cors = require("micro-cors")()
-const isDev = process.env.NOW_REGION === "dev1"
+const cors = require("micro-cors")();
+// const isDev = process.env.NOW_REGION === "dev1";
+const isDev = process.env.NODE_ENV === "development";
 
 const typeDefs = gql`
-    type Query {
-        page(url: String!): Website
-    }
+  type Query {
+    page(url: String!): Website
+  }
 
-    type Website {
-        g(query: String!): [DOMNode]
-    }
+  type Website {
+    g(query: String!): [DOMNode]
+  }
 
-    type DOMNode {
-        html: String
-        text: String
-        val: String
-        attr(name: String!): String
-        first(query: String!): DOMNode
-        find(query: String!): [DOMNode]
-    }
-`
+  type DOMNode {
+    html: String
+    text: String
+    val: String
+    attr(name: String!): String
+    first(query: String!): DOMNode
+    find(query: String!): [DOMNode]
+  }
+`;
 
 const resolvers = {
-    Query: {
-        page: async (_parent, { url }, { isDev, getHTML, cheerio }) => {
-            const html = await getHTML({ isDev, url })
+  Query: {
+    page: async (_parent, { url }, { isDev, getHTML, cheerio }) => {
+      const html = await getHTML({ isDev, url });
 
-            return cheerio.load(html)
-        },
+      return cheerio.load(html);
     },
-    Website: {
-        g: ($, { query }, _context) => {
-            return $(query).toArray()
-        },
+  },
+  Website: {
+    g: ($, { query }, _context) => {
+      return $(query).toArray();
     },
-    DOMNode: {
-        html: (g, _args, { cheerio }) => {
-            return cheerio(g).html()
-        },
-        text: (g, _args, { cheerio }) => {
-            return cheerio(g).text()
-        },
-        val: (g, _args, { cheerio }) => {
-            return cheerio(g).val()
-        },
-        attr: (g, { name }, { cheerio }) => {
-            return cheerio(g).attr(name)
-        },
-        find: (g, { query }, { cheerio }) => {
-            return cheerio(g).find(query).toArray()
-        },
-        first: (g, { query }, { cheerio }) => {
-            return cheerio(g).find(query).first()
-        },
+  },
+  DOMNode: {
+    html: (g, _args, { cheerio }) => {
+      return cheerio(g).html();
     },
-}
+    text: (g, _args, { cheerio }) => {
+      return cheerio(g).text();
+    },
+    val: (g, _args, { cheerio }) => {
+      return cheerio(g).val();
+    },
+    attr: (g, { name }, { cheerio }) => {
+      return cheerio(g).attr(name);
+    },
+    find: (g, { query }, { cheerio }) => {
+      return cheerio(g).find(query).toArray();
+    },
+    first: (g, { query }, { cheerio }) => {
+      return cheerio(g).find(query).first();
+    },
+  },
+};
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    introspection: true,
-    playground: true,
-    context: (integrationContext) => ({
-        ...integrationContext,
-        cheerio,
-        getHTML,
-        isDev,
-    }),
-})
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true,
+  context: (integrationContext) => ({
+    ...integrationContext,
+    cheerio,
+    getHTML,
+    isDev,
+  }),
+});
 
 exports.config = {
-    api: { bodyParser: false },
-}
+  api: { bodyParser: false },
+};
 
 module.exports = cors((req, res, ...args) => {
-    if (req.method === "OPTIONS") {
-        return res.status(200).send()
-    }
+  if (req.method === "OPTIONS") {
+    return res.status(200).send();
+  }
 
-    const handler = server.createHandler({ path: "/api" })
+  const handler = server.createHandler({ path: "/api" });
 
-    return handler(req, res, ...args)
-})
+  return handler(req, res, ...args);
+});
